9a172ff8b7ac8506d372680050fceac744f4d166
ElaineMoreira <seuemail@exemplo.br>
2021-05-24 18:45:30 -0300

    adiciona exercicios feitos da parte 1

---
+++ bloco_4/dia_4/part1/exercises2.js
@@ -0,0 +1,15 @@
+//Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, 
+//em seguida, imprima o objeto no console.
+let objeto = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    recorrente: 'sim'
+  };
+
+  for (let key in objeto){
+      console.log(objeto);
+    };
+
+  
+  
---
+++ bloco_4/dia_4/part1/exercises3.js
@@ -0,0 +1,11 @@
+let objeto = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    recorrente: 'sim'
+  };
+  for (let key in objeto) {
+      console.log(key)
+      }
+  
+  
